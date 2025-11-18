71b5619f30d6e74aafb119a7ba5cad1f32cb53ca
Vishalvasvsni <154272722+Vishalvasvsni@users.noreply.github.com>
2025-11-18 16:15:04 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -117,7 +117,7 @@ async function displayAlbums() {
 
 async function main() {
     // Get the list of all the songs
-    await getSongs("songs/ncs")
+    await getSongs("songs/ncs/sapne-bade-305719.mp3")
     playMusic(songs[0], true)
 
     // Display all the albums on the page
