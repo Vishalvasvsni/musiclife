655c0d2d34dd2d7c0df633da423c793dd00ca0bc
Vishalvasvsni <154272722+Vishalvasvsni@users.noreply.github.com>
2025-11-18 16:08:31 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -29,6 +29,7 @@ async function getSongs(folder) {
         const element = as[index];
         if (element.href.endsWith(".mp3")) {
             songs.push(element.href.split(`/${folder}/`)[1])
+            console.log(songs);
         }
     }
  
